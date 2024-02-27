ba12f426c0fad66452b861b8d65d94c0d1aacdc0
charl11e <146877892+charl11e@users.noreply.github.com>
2024-02-27 12:06:39 +0000

    Cleaned up app functions, added error handling

--- README.md
+++ README.md
@@ -6,9 +6,21 @@
 # Code References
 Bootstrap Team (2023a) Bootstrap Download. Available at: https://getbootstrap.com/docs/5.3/getting-started/download/ (Accessed: 25th February 2024)
 
+DigitalOcean (2020) How to use the res Object in Express. Available at: https://www.digitalocean.com/community/tutorials/nodejs-res-object-in-expressjs (Accessed: 25th February 2024)
+
+MDN Web Docs (2023a) Arrow function expressions. Available at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions (Accessed: 27th February 2024)
+
+MDN Web Docs (2023b) Control flow and error handling. Available at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling (Accessed: 27th February 2024)
+
+MDN Web Docs (2023c) HTTP response status codes. Available at: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses (Accessed: 27th February 2024)
+
+StackAbuse (2023) How to return status codes in Express. Available at: https://stackabuse.com/bytes/how-to-return-status-codes-in-express/ (Accessed: 27th February 2024)
+
 W3Schools (Refsnes Data) (2023a) HTML Favicon. Available at: https://www.w3schools.com/html/html_favicon.asp (Accessed: 25th February 2024)
 
 W3Schools (Refsnes Data) (2023b) HTML Responsive Web Design. Available at: https://www.w3schools.com/html/html_responsive.asp (Accessed 25th February 2024)
+
+W3Schools (Refsnes Data) (2023c) Javascript Errors. Available at: https://www.w3schools.com/js/js_errors.asp (Accessed 27th February 2024)
 
 # Image References
 favicon
--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-// Load ingredients and recipes
-const ingredients = require('./ingredients');
-const recipes = require('./recipes');
+// All HTTP response status codes are used from MDN Web Docs (2023c)
+
+// Load ingredients and recipes (W3Schools, 2023c)
+let ingredients, recipes;
+try {
+    ingredients = require('./ingredients');
+    recipes = require('./recipes');
+} catch (err) {
+    console.error('Error loading ingredients and recipes, data will be empty and may not be saved', err);
+    ingredients = [];
+    recipes = [];
+};
 
 // Set up express and file system
 const express = require('express');
@@ -10,6 +19,18 @@ const fs = require('fs');
 // Load pages statically from client folder
 app.use(express.static('client'));
 
+// Write to file function (MDN Web Docs, 2023a, 2023b) (StackAbuse, 2023) (DigitalOcean, 2020)
+function write (filename, data, res) {
+    fs.writeFile(filename, JSON.stringify(data), (err) => {
+        if (err) {
+            console.error('Error writing to file', err);
+            res.status(500).send('Error writing to file');
+            return;
+        }
+    res.send('Successfully added!');
+    });
+}
+
 // Add new recipes and ingredients
 app.post('/new-recipe', (req, res) => {
     const title = req.body.title;
@@ -17,18 +38,34 @@ app.post('/new-recipe', (req, res) => {
     const servings = req.body.servings;
     const instructions = req.body.instructions;
     recipes.push({ title, servings, ingredients, instructions });
-    fs.writeFileSync('./recipes.json', JSON.stringify(recipes));
-    res.send('Recipe added!');
+    write('./recipes.json', recipes, res);
 });
 
 app.post('/new-ingredient', (req, res) => {
     // Check if ingredient already exists
     if (ingredients.includes(req.body.ingredient)) {
-        res.send('Ingredient already exists');
+        res.status(400).send('Ingredient already exists');
+        return;
     }
     ingredients.push(req.body.ingredient);
-    fs.writeFileSync('./ingredients.json', JSON.stringify(ingredients));
-    res.send('Ingredient added!');
+    write('./ingredients.json', ingredients, res);
+});
+
+// Initialise ingredients and recipes
+app.post('/initialise', (req, res) => {
+    console.log('IMPLEMENT ME');
+});
+
+// Look for recipes that use specific ingredients (NEEDS TO BE IMPLEMENTED PROPERLY)
+app.get('/match-recipes', (req, res) => {
+    const ingredientssearch = req.query.ingredients;
+    let matchingrecipes = [];
+    for (let i in recipes) {
+        if (recipes[i].ingredients.includes(ingredientssearch)) {
+            matchingrecipes.push(recipes[i]);
+        }
+    }
+    res.send(matchingrecipes);
 });
 
 module.exports = app;
